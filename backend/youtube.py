0318891ec0c110f945ac91ac1f8e36a1a0bcc17f
harshjain262004 <harshjain262004@gmail.com>
2025-03-05 03:55:29 +0530

    Circular Switching of API keys to prevent exhaustion

--- backend/youtube.py
+++ backend/youtube.py
@@ -5,14 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 from Db import getQueries, AddVideos
 
-api_service_name = "youtube"
-api_version='v3'
-developer_key = os.environ["YOUTUBE_API_KEY_1"]
-youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=developer_key)
-
-
-# for now cricket is hardcoded, we can make it dynamic by fetching queries from db
 def YoutubeAPIScript():
+    keyIndex = getIndexFromFile()
+    api_service_name = "youtube"
+    api_version='v3'
+    developer_key = os.environ[f"YOUTUBE_API_KEY_{keyIndex}"]
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=developer_key)
     queries = getQueries()
     if len(queries) == 0:
         print("No queries present in the database")
@@ -20,7 +18,7 @@ def YoutubeAPIScript():
     for id,query in queries:
         published_after = (datetime.now(timezone.utc) - timedelta(minutes=5)).replace(microsecond=0).strftime("%Y-%m-%dT%H:%M:%SZ")
         request = youtube.search().list(
-            q="Cricket",
+            q=query,
             part="snippet",
             maxResults=50,
             type="video",
@@ -29,5 +27,18 @@ def YoutubeAPIScript():
         )
         response = request.execute()
         AddVideos(response,id)
+    updateKeyIndex((keyIndex+1)%15)
     print("Script Completion")
     return "Script Completion"
+
+def getIndexFromFile():
+    with open("keyIndex.txt","r") as f:
+        if f.read() == "":
+            return 0
+        keyIndex = int(f.read())
+    return keyIndex
+
+def updateKeyIndex(keyIndex):
+    with open("keyIndex.txt","w") as f:
+        f.write(str(keyIndex))
+    return
