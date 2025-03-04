5d014d3b51e06d374f132d229a4ed50dfe06b1cb
harshjain262004 <harshjain262004@gmail.com>
2025-03-05 04:29:48 +0530

    Reverse Chrono ordered Data Fetching

--- backend/Db.py
+++ backend/Db.py
@@ -29,3 +29,50 @@ def AddVideos(response,query_id):
         connection.commit()
     print("Videos added to the database: ", len(videos))
     return "Videos added to the database"
+
+def fetchVideos(page, limit):
+    offset = (page - 1) * limit
+    query = """
+        SELECT * FROM queries INNER JOIN videos ON videos.query_id = queries.id 
+        ORDER BY published_at DESC 
+        LIMIT %s OFFSET %s;
+    """
+    cursor.execute(query, (limit, offset))
+    rows = cursor.fetchall()
+    return [
+        {  
+            "query": row[1],
+            "video_id": row[2],
+            "title": row[3],
+            "description": row[4],
+            "published_at": row[5],
+            "channel_id": row[6],
+            "channel_title": row[7],
+            "thumbnails": row[8],
+        }
+        for row in rows
+    ]
+
+def fetchVideosQuery(page, limit, query_str):
+    offset = (page - 1) * limit
+    query = """
+        SELECT * FROM queries INNER JOIN videos ON videos.query_id = queries.id
+        WHERE query = %s 
+        ORDER BY published_at DESC 
+        LIMIT %s OFFSET %s;
+    """
+    cursor.execute(query, (query_str, limit, offset))
+    rows = cursor.fetchall()
+    return [
+        {  
+            "query": row[1],
+            "video_id": row[2],
+            "title": row[3],
+            "description": row[4],
+            "published_at": row[5],
+            "channel_id": row[6],
+            "channel_title": row[7],
+            "thumbnails": row[8],
+        }
+        for row in rows
+    ]
--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
-import os
 from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask, request, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
 from youtube import *
+from Db import *
 
 app = Flask(__name__)
 app.secret_key = "Youtube API project"
@@ -17,6 +17,23 @@ scheduler.start()
 def homepage():
     return "Youtube API project"
 
+@app.route('/api', methods=['GET'])
+def api():
+    try:
+        page = int(request.args.get("page", 1))
+        limit = int(request.args.get("limit", 10))
+        query = request.args.get("query",None)
+        if page < 1 or limit < 1:
+            return jsonify({"error": "Invalid page or limit"}), 400
+        if query:
+            videos = fetchVideosQuery(page, limit, query)
+            return jsonify({"page": page, "limit": limit, "videos": videos})
+        videos = fetchVideos(page, limit)
+        return jsonify({"page": page, "limit": limit, "videos": videos})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
